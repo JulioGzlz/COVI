8fe3e7aaca909cc2f625afca8174da9b84c09feb
Edgar <landeroej@gmail.com>
2020-03-15 19:46:28 -0600

    Se crean nuevos componentes

--- vGLEZ/src/app/app-routing.module.ts
+++ vGLEZ/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoCaracteristicasComponent } from './todo/todo-caracteristicas/todo-caracteristicas.component';
+import { CasaComponent } from './casa/casa.component';
+import { PreciosComponent } from './precios/precios.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'precios', component: PreciosComponent},
+  {path: 'casa', component: CasaComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- vGLEZ/src/app/app.module.ts
+++ vGLEZ/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { TodoCaracteristicasComponent } from './todo/todo-caracteristicas/todo-caracteristicas.component';
+import { CasaComponent } from './casa/casa.component';
+import { MenuComponent } from './menu/menu.component';
+import { PreciosComponent } from './precios/precios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoCaracteristicasComponent,
+    CasaComponent,
+    MenuComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
