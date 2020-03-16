0ce33b1a4ef8e111032ba0f209c5ce609d19afdd
Edgar <landeroej@gmail.com>
2020-03-15 18:25:52 -0600

    vGLEZ

--- vGLEZ/src/app/app.module.ts
+++ vGLEZ/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {  NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,6 @@ import {  NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
     AppRoutingModule
   ],
   providers: [],
